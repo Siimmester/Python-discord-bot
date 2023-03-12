06b5e498debe01f3b97fafd6875794235ae9dac4
Siimmester <siimmar06@gmail.com>
2023-03-12 21:01:41 +0200

    Idk wtf am i doing

--- bott.py
+++ bott.py
@@ -11,15 +11,16 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
     await client.change_presence(activity=discord.Game("$play for fun :)"))
 
+
 @client.tree.command(name="play", description='Play a game of "higher or lower"')
 async def play(interaction: discord.Interaction):
-    await interaction.response.send_message("{placeholder}")
+    a = str(interaction.guild.get_member(247320522814259200))
+    await interaction.response.send_message(a[:-5:])
 
 
 @client.command()
 async def play(ctx, *args):
-    arguments = ', '.join(args)
-    await ctx.send(f'{len(args)} arguments: {arguments}')
+    await ctx.send(f'{ctx.guild.get_member(243320522814259200)}')
 
 
 client.run(tokens.Discord_Token)
