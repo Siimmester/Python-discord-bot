eca22bad60fdfc3d58c800b49efb29720b36bb3e
Siimmester <siimmar06@gmail.com>
2023-03-13 17:17:53 +0200

    Idk wtf am i doing

--- bott.py
+++ bott.py
@@ -1,5 +1,7 @@
 import tokens
 import discord
+# import random
+# from monthlyVolume import mVolList
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='$', intents=discord.Intents.all())
@@ -9,18 +11,31 @@ client = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 async def on_ready():
     await client.tree.sync()
     print(f'We have logged in as {client.user}')
-    await client.change_presence(activity=discord.Game("$play for fun :)"))
+    await client.change_presence(activity=discord.Game("$play or /play for fun :)"))
+
+
+class Buttons(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.value = None
+
+    @discord.ui.button(label="Higher", style=discord.ButtonStyle.green)
+    async def higher(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.channel.send(content="Higher")
+
+    @discord.ui.button(label="Lower", style=discord.ButtonStyle.red)
+    async def lower(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.channel.send(content="Lower")
 
 
 @client.tree.command(name="play", description='Play a game of "higher or lower"')
 async def play(interaction: discord.Interaction):
-    a = str(interaction.guild.get_member(247320522814259200))
-    await interaction.response.send_message(a[:-5:])
+    await interaction.response.send_message(content="Higher or Lower?", view=Buttons())
 
 
 @client.command()
-async def play(ctx, *args):
-    await ctx.send(f'{ctx.guild.get_member(243320522814259200)}')
+async def play(ctx, arg):
+    await ctx.send(f'{ctx.guild.get_member(int(arg))}')
 
 
 client.run(tokens.Discord_Token)
