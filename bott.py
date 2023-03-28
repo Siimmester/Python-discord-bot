fead860f31ab1ecf4a5c8ccb43b1a94c211123ce
Siimmester <siimmar06@gmail.com>
2023-03-28 17:48:20 +0300

    v 0.99

--- bott.py
+++ bott.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 
 
+# once the bot boots up, it will print in to the console and the bot will start playing the game "$play or /play for
+# fun :)"
 @client.event
 async def on_ready():
     await client.tree.sync()
@@ -14,76 +16,106 @@ async def on_ready():
     await client.change_presence(activity=discord.Game("$play or /play for fun :)"))
 
 
+# Basic view
 class Buttons(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
         self.user_higher = None
 
+    # Will make the variable user_higher True and should stop the view
     @discord.ui.button(label="Higher", style=discord.ButtonStyle.green)
     async def higher(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.user_higher = True
         self.stop()
 
+    # Will make the variable user_higher False and should stop the view
     @discord.ui.button(label="Lower", style=discord.ButtonStyle.red)
     async def lower(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.user_higher = False
         self.stop()
 
 
+# Slash command
 @client.tree.command(name="play", description='Play a game of "higher or lower"')
 async def play(interaction: discord.Interaction):
-    a = random.randint(0, 66)
-    c = random.randint(0, 66)
-    while a == c or mVolList[a][2] == mVolList[c][2]:
-        c = random.randint(0, 66)
+    # Generates 2 random ints
+    random_list_item_1 = random.randint(0, 66)
+    random_list_item_2 = random.randint(0, 66)
+    # Checks if the two random ints are the same and if so replaces it.
+    while random_list_item_1 == random_list_item_2 or mVolList[random_list_item_1][2] == mVolList[random_list_item_2][
+        2]:
+        random_list_item_2 = random.randint(0, 66)
+    # Int to add to if user gets answer right
     score_counter = 0
-    have_lost = True
-    await interaction.channel.send(f"---")
-    while have_lost:
-        buttons = Buttons()
-        await interaction.channel.send(
-            f"`Which is more popular: \n {mVolList[a][0]} --- {mVolList[a][1]} searches a month --- or --- {mVolList[c][0]} --- ? searches a month`")
-        await interaction.channel.send(content=f"Is *{mVolList[c][0]}* Higher or Lower?", view=buttons)
-        await buttons.wait()
-        if (mVolList[a][2] < mVolList[c][2]) == buttons.user_higher:
-            await interaction.channel.send(content=f'`you are correct {mVolList[c][1]}`')
+    # Boolean to modify if user gets answer wrong
+    have_not_lost = True
+    # Using interaction.channel.send so slash command doesn't give error
+    await interaction.response.send_message(f"---")
 
+    while have_not_lost:
+        buttons = Buttons()
+        # Send message to channel that user commanded
+        await interaction.channel.send(
+            f"`Which is more popular: \n {mVolList[random_list_item_1][0]} --- {mVolList[random_list_item_1][1]} searches a month --- or --- {mVolList[random_list_item_2][0]} --- ? searches a month`")
+        await interaction.channel.send(content=f"Is *{mVolList[random_list_item_2][0]}* Higher or Lower?", view=buttons)
+        await buttons.wait()
+        # if the button pressed gives the right variable give score counter + 1 and print out that the user was right
+        if (mVolList[random_list_item_1][2] < mVolList[random_list_item_2][2]) == buttons.user_higher:
+            await interaction.channel.send(content=f'`you are correct {mVolList[random_list_item_2][1]}`')
             score_counter += 1
+        # Else end the game
         else:
-            have_lost = False
-        a = c
-        c = random.randint(0, 66)
-        while a == c or mVolList[a][2] == mVolList[c][2]:
-            c = random.randint(0, 66)
+            have_not_lost = False
+
+        random_list_item_1 = random_list_item_2
+        random_list_item_2 = random.randint(0, 66)
+        # Checks if the two random ints are the same and if so replaces it.
+        while random_list_item_1 == random_list_item_2 or mVolList[random_list_item_1][2] == \
+                mVolList[random_list_item_2][2]:
+            random_list_item_2 = random.randint(0, 66)
+    # after leaving the loop print out
     await interaction.channel.send(f"You got {str(score_counter)} Points")
     await interaction.channel.send(f"---")
 
 
+# text box command started by $
 @client.command()
 async def play(ctx):
-    a = random.randint(0, 66)
-    c = random.randint(0, 66)
-    while a == c or mVolList[a][2] == mVolList[c][2]:
-        c = random.randint(0, 66)
+    # Generates 2 random ints
+    random_list_item_1 = random.randint(0, 66)
+    random_list_item_2 = random.randint(0, 66)
+    # Checks if the two random ints are the same and if so replaces it.
+    while random_list_item_1 == random_list_item_2 or mVolList[random_list_item_1][2] == mVolList[random_list_item_2][
+        2]:
+        random_list_item_2 = random.randint(0, 66)
+    # Int to add to if user gets answer right
     score_counter = 0
-    have_lost = True
+    # Boolean to modify if user gets answer wrong
+    have_not_lost = True
+    # not needed, just to stay consistent
     await ctx.send(f"---")
-    while have_lost:
-        buttons = Buttons()
-        await ctx.send(
-            f"`Which is more popular: \n {mVolList[a][0]} --- {mVolList[a][1]} searches a month --- or --- {mVolList[c][0]} --- ? searches a month`")
-        await ctx.send(content=f"Is *{mVolList[c][0]}* Higher or Lower?", view=buttons)
-        await buttons.wait()
-        if (mVolList[a][2] < mVolList[c][2]) == buttons.user_higher:
-            await ctx.send(content=f'`you are correct {mVolList[c][1]}`')
 
+    while have_not_lost:
+        buttons = Buttons()
+        # Send message to channel that user commanded
+        await ctx.send(
+            f"`Which is more popular: \n {mVolList[random_list_item_1][0]} --- {mVolList[random_list_item_1][1]} searches a month --- or --- {mVolList[random_list_item_2][0]} --- ? searches a month`")
+        await ctx.send(content=f"Is *{mVolList[random_list_item_2][0]}* Higher or Lower?", view=buttons)
+        await buttons.wait()
+        # if the button pressed gives the right variable give score counter + 1 and print out that the user was right
+        if (mVolList[random_list_item_1][2] < mVolList[random_list_item_2][2]) == buttons.user_higher:
+            await ctx.send(content=f'`you are correct {mVolList[random_list_item_2][1]}')
             score_counter += 1
+        # Else end the game
         else:
-            have_lost = False
-        a = c
-        c = random.randint(0, 66)
-        while a == c or mVolList[a][2] == mVolList[c][2]:
-            c = random.randint(0, 66)
+            have_not_lost = False
+
+        random_list_item_1 = random_list_item_2
+        random_list_item_2 = random.randint(0, 66)
+        while random_list_item_1 == random_list_item_2 or mVolList[random_list_item_1][2] == \
+                mVolList[random_list_item_2][2]:
+            random_list_item_2 = random.randint(0, 66)
+
     await ctx.send(f"You got {str(score_counter)} Points")
     await ctx.send(f"---")
 
